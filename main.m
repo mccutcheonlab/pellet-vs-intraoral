f4b9045357821f9e026468401225edaef873c1a0
Jaime McCutcheon <jem64@le.ac.uk>
2018-07-26 13:10:27 +0100

    Updated with rat details

--- main.m
+++ main.m
@@ -3,19 +3,20 @@
 % masterfolder = 'R:\DA_and_Reward\jem64\Publication Data\McCutcheon_ACSChem\'
 
 %% Extracts all data for individual rats
-extractdata('pvi10', 10, 120927, 1, '!2012-09-27_13h54m.Subject PVI10', 475.669)
+extractdata('pvi08', 8, 120925, 1, '!2012-09-27_13h54m.Subject PVI10', 225.835)
 
-% extractdata('pvi10', 10, 120927, 1, '!2012-09-27_13h54m.Subject PVI10', 475.669)
+% extractdata('pvi09', 9, 120926, 1, '!2012-09-26_11h23m.Subject PVI9', 472.082)
 % 
 % extractdata('pvi10', 10, 120927, 1, '!2012-09-27_13h54m.Subject PVI10', 475.669)
 % 
-% extractdata('pvi10', 10, 120927, 1, '!2012-09-27_13h54m.Subject PVI10', 475.669)
+% extractdata('pvi12', 12, 121129, 1, '!2012-11-29_13h47m.Subject PVI12', 491.02)
 % 
-% extractdata('pvi10', 10, 120927, 1, '!2012-09-27_13h54m.Subject PVI10', 475.669)
+% extractdata('pvi15', 15, 121218, 1, '!2012-12-18_11h00m.Subject PVI15', 1359.28)
 % 
-% extractdata('pvi10', 10, 120927, 1, '!2012-09-27_13h54m.Subject PVI10', 475.669)
+% extractdata('pvi16', 16, 121218, 2, '!2012-12-18_15h02m.Subject PVI16'', 213.765)
 % 
-% extractdata('pvi10', 10, 120927, 1, '!2012-09-27_13h54m.Subject PVI10', 475.669)
+% extractdata('pvi18', 18, 121219, 2, '!2012-12-19_12h48m.Subject PVI18', 138.501)
+
 
 %% Combines data into single matrix for further analysis
 
